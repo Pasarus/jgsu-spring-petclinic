99efacf25b9ed6b6fe2238a09841e8489a93ad06
Samuel Jones <samuel.jones@stfc.ac.uk>
2020-12-11 10:57:15 +0000

    Revert "Revert "break a test""
    This reverts commit d18f22a419c770767748385bfc08e8be3177b003.

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,8 +45,8 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("asdasdasdasd");
-		person.setLastName("smih");
+		person.setFirstName("notempty");
+		person.setLastName("smith");
 
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
